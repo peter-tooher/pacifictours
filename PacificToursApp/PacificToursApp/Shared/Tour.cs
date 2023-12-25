81b6aa6bbdad55afd3c8751a3f1168dc40a6d2c7
Peter Tooher <peter.tooher@warwick.ac.uk>
2023-12-25 23:20:34 +0000

    feat(shared): Add tour model

---
+++ PacificToursApp/PacificToursApp/Shared/Tour.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacificToursApp.Shared
+{
+    public class Tour
+    {
+        public int TourId { get; set; }
+        public string TourName { get; set; } = string.Empty;
+        public string TourDescription { get; set; } = string.Empty;
+        public string TourImageUrl { get; set; } = string.Empty;
+        public int TourLength { get; set; }
+        public decimal TourPrice { get; set; }
+
+        public int TourAvailableSpaces { get; set; }
+    }
+}
