a196f72b0787ade45931308cc12d34eb9deb5e5b
Peter Tooher <peter.tooher@warwick.ac.uk>
2023-12-27 19:05:11 +0000

    feat(../Server/Program.cs): Register the DbContext and configure SqlServer

--- PacificToursApp/PacificToursApp/Server/Program.cs
+++ PacificToursApp/PacificToursApp/Server/Program.cs
@@ -1,10 +1,16 @@
 global using PacificToursApp.Shared;
 global using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.ResponseCompression;
+using PacificToursApp.Server.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
