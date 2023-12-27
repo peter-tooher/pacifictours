7596a76ba0e42c8379c504d9295358ab86f8c07d
Peter Tooher <peter.tooher@warwick.ac.uk>
2023-12-27 19:35:11 +0000

    feat(DbContext): Add DataContext

---
+++ PacificToursApp/PacificToursApp/Server/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Identity.Client;
+
+namespace PacificToursApp.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        { 
+
+        }
+
+        public DbSet<Tour> Tours { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+    }
+}
