e7741f2cfce376ef067af0275bac99aec6ea5cb6
Peter Tooher <peter.tooher@warwick.ac.uk>
2023-12-28 15:30:11 +0000

    feat: Add digits for decimial datatype

--- PacificToursApp/PacificToursApp/Shared/Tour.cs
+++ PacificToursApp/PacificToursApp/Shared/Tour.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,7 @@ namespace PacificToursApp.Shared
         public string TourDescription { get; set; } = string.Empty;
         public string TourImageUrl { get; set; } = string.Empty;
         public int TourLength { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
         public decimal TourPrice { get; set; }
 
         public int TourAvailableSpaces { get; set; }
