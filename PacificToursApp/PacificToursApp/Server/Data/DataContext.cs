a5b45fe1c91ecf981935a4166e75491bec1b3005
Peter Tooher <peter.tooher@warwick.ac.uk>
2023-12-28 15:35:23 +0000

    feat(DbContext): Add separate suite types

--- PacificToursApp/PacificToursApp/Server/Data/DataContext.cs
+++ PacificToursApp/PacificToursApp/Server/Data/DataContext.cs
@@ -11,5 +11,8 @@ namespace PacificToursApp.Server.Data
 
         public DbSet<Tour> Tours { get; set; }
         public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<SingleSuite> SingleSuites { get; set; }
+        public DbSet<DoubleSuite> DoubleSuites { get; set; }
+        public DbSet<FamilySuite> FamilySuites { get; set; }
     }
 }
