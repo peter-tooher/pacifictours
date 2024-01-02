ff9f61510eadb3008562c0f8a868ee3c015f0dbd
Peter Tooher <peter.tooher@warwick.ac.uk>
2024-01-02 17:00:13 +0000

    feat(TourService): implement ITourService and add GetTours method
    This commit introduces the TourService class in the TourService namespace. It implements the ITourService interface and includes a GetTours method, which fetches tour data from the API and assigns it to the Tours property.

---
+++ PacificToursApp/PacificToursApp/Client/Services/TourService/TourService.cs
@@ -0,0 +1,20 @@
+﻿namespace PacificToursApp.Client.Services.TourService
+{
+    public class TourService : ITourService
+    {
+        private readonly HttpClient _http;
+        public TourService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public List<Tour> Tours { get; set; } = new List<Tour>();
+
+        public async Task GetTours()
+        {
+            var result = await _http.GetFromJsonAsync<ServiceResponse<List<Tour>>>("api/tour");
+            if (result != null && result.Data != null) 
+                Tours = result.Data;
+        }
+    }
+}
