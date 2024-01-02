07cc3bd518da8c94fcb078fff36fbd3966150a60
Peter Tooher <peter.tooher@warwick.ac.uk>
2024-01-02 16:55:27 +0000

    feat(ITourService): add Tours property and GetTours method
    This commit introduces the ITourService interface in the TourService namespace. It includes a Tours property and a GetTours method, which are essential for managing tour data in the application.

---
+++ PacificToursApp/PacificToursApp/Client/Services/TourService/ITourService.cs
@@ -0,0 +1,10 @@
+﻿using PacificToursApp.Shared;
+
+namespace PacificToursApp.Client.Services.TourService
+{
+    public interface ITourService
+    {
+        List<Tour> Tours { get; set; }
+        Task GetTours();
+    }
+}
