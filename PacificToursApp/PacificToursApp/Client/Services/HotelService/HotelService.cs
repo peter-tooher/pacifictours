accbceefce55cd19d6a8a99bc89b2bb5dede863f
Peter Tooher <peter.tooher@warwick.ac.uk>
2024-01-02 17:45:36 +0000

    feat(HotelService): implement IHotelService and add GetHotels method
    This commit introduces the HotelService class in the HotelService namespace. It implements the IHotelService interface and includes a GetHotels method, which fetches hotel data from the API and assigns it to the Hotels property.

---
+++ PacificToursApp/PacificToursApp/Client/Services/HotelService/HotelService.cs
@@ -0,0 +1,22 @@
+﻿using PacificToursApp.Shared;
+
+namespace PacificToursApp.Client.Services.HotelService
+{
+    public class HotelService : IHotelService
+    {
+        private readonly HttpClient _http;
+        public HotelService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public List<Hotel> Hotels { get; set; } = new List<Hotel>();
+
+        public async Task GetHotels()
+        {
+            var result = await _http.GetFromJsonAsync<ServiceResponse<List<Hotel>>>("api/Hotel");
+            if (result != null && result.Data != null)
+                Hotels = result.Data;
+        }
+    }
+}
