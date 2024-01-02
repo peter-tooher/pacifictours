fa5dd30db8f723146e5f25adc7cee138ffe47abb
Peter Tooher <peter.tooher@warwick.ac.uk>
2024-01-02 18:10:04 +0000

    feat(Program.cs): add scoped services for ITourService and IHotelService
    This commit adds scoped services for ITourService and IHotelService to the Program.cs file. This allows these services to be used throughout the application with dependency injection.

--- PacificToursApp/PacificToursApp/Client/Program.cs
+++ PacificToursApp/PacificToursApp/Client/Program.cs
@@ -1,3 +1,7 @@
+global using PacificToursApp.Shared;
+global using System.Net.Http.Json;
+global using PacificToursApp.Client.Services.TourService;
+global using PacificToursApp.Client.Services.HotelService;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PacificToursApp.Client;
@@ -7,5 +11,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<ITourService, TourService>();
+builder.Services.AddScoped<IHotelService, HotelService>();
 
 await builder.Build().RunAsync();
