379458f4cdcbc6b5700d51e44ed8f7c18b1bebd4
Peter Tooher <peter.tooher@warwick.ac.uk>
2024-01-02 13:45:07 +0000

    feat(TourService): add GetToursAsync method
    This commit adds the GetToursAsync method to the TourService class. This method fetches all Tour objects from the database asynchronously and returns them as a list wrapped in a ServiceResponse object.

---
+++ PacificToursApp/PacificToursApp/Server/Services/TourService/TourService.cs
@@ -0,0 +1,21 @@
+﻿namespace PacificToursApp.Server.Services.TourService
+{
+    public class TourService : ITourService
+    {
+        private readonly DataContext _context;
+
+        public TourService(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<ServiceResponse<List<Tour>>> GetToursAsync()
+        {
+            var response = new ServiceResponse<List<Tour>>
+            {
+                Data = await _context.Tours.ToListAsync()
+            };
+
+            return response;
+        }
+    }
+}
