2b5938b4ab7b16689b0f27834683cac5c78f237f
Peter Tooher <peter.tooher@warwick.ac.uk>
2024-01-03 16:00:02 +0000

    feat(NameAttribute): add name validation in PacificToursApp.Shared namespace
    This commit introduces the NameAttribute class in the PacificToursApp.Shared namespace. The class extends ValidationAttribute and overrides the IsValid method to validate that a name only contains letters.

---
+++ PacificToursApp/PacificToursApp/Shared/NameAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PacificToursApp.Shared
+{
+
+
+
+    public class NameAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                string name = value.ToString();
+                Regex regex = new Regex(@"^[a-zA-Z]+$"); 
+                if (!regex.IsMatch(name))
+                {
+                    return new ValidationResult("Invalid name. Only letters are allowed.");
+                }
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
