8d1186fad91b406f0cb0ddfbd2aef4ab022f9674
Peter Tooher <peter.tooher@warwick.ac.uk>
2024-01-03 19:00:06 +0000

    feat(AuthService): enhance Register method in AuthService class
    This commit enhances the Register method in the AuthService class within the PacificToursApp.Server.Services.AuthService namespace. The method now includes password hashing and salt generation, as well as user creation in the DataContext. This enhancement improves the security and functionality of user registration.

--- PacificToursApp/PacificToursApp/Server/Services/AuthService/AuthService.cs
+++ PacificToursApp/PacificToursApp/Server/Services/AuthService/AuthService.cs
@@ -1,25 +1,58 @@
-﻿namespace PacificToursApp.Server.Services.AuthService
+﻿using System.Security.Cryptography;
+
+namespace PacificToursApp.Server.Services.AuthService
 {
     public class AuthService : IAuthService
     {
         private readonly DataContext _context;
-        public AuthService (DataContext context)
+        public AuthService(DataContext context)
         {
             _context = context;
         }
-        public Task<ServiceResponse<int>> Register(User user, string password)
+        public async Task<ServiceResponse<int>> Register(User user, string password)
         {
-            throw new NotImplementedException();
+            if (await UserExists(user.UserName))
+            {
+                return new ServiceResponse<int>
+                {
+                    Success = false,
+                    Message = "User already exists."
+                };
+            }
+
+            CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return new ServiceResponse<int>
+            {
+                Data = user.UserId,
+                Message = "User created successfully."
+            };
         }
 
         public async Task<bool> UserExists(string username)
         {
-            if(await _context.Users.AnyAsync(user => user.UserName.ToLower()
+            if (await _context.Users.AnyAsync(user => user.UserName.ToLower()
             .Equals(username.ToLower())))
             {
                 return true;
             }
             return false;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac
+                .ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
         }
     }
 }
