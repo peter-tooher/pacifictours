46a0a62c5fe0b6101c06ba7340bdd6de84ad515e
Peter Tooher <peter.tooher@warwick.ac.uk>
2024-01-03 18:20:02 +0000

    feat(AuthService): implement AuthService class with Register and UserExists methods
    This commit implements the AuthService class with Register and UserExists methods in the AuthService namespace within the PacificToursApp.Server.Services. This implementation allows for user registration and existence check functionality.

---
+++ PacificToursApp/PacificToursApp/Server/Services/AuthService/AuthService.cs
@@ -0,0 +1,25 @@
+﻿namespace PacificToursApp.Server.Services.AuthService
+{
+    public class AuthService : IAuthService
+    {
+        private readonly DataContext _context;
+        public AuthService (DataContext context)
+        {
+            _context = context;
+        }
+        public Task<ServiceResponse<int>> Register(User user, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> UserExists(string username)
+        {
+            if(await _context.Users.AnyAsync(user => user.UserName.ToLower()
+            .Equals(username.ToLower())))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
