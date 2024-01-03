fdf454fdf59e11f01d2334b20bd541e248089a7f
Peter Tooher <peter.tooher@warwick.ac.uk>
2024-01-03 16:50:06 +0000

    feat(User): create User class in PacificToursApp.Shared namespace
    This commit introduces the User class in the PacificToursApp.Shared namespace. The User class includes properties for UserId, UserName, FirstName, LastName, UserEmail, PassportNumber, ContactNumber, PasswordHash, PasswordSalt, and DateCreated.

---
+++ PacificToursApp/PacificToursApp/Shared/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacificToursApp.Shared
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string UserEmail { get; set; } = string.Empty;
+        public int PassportNumber { get; set; }
+        public string ContactNumber { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+    }
+}
