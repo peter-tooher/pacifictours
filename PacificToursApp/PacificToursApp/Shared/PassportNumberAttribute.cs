1ead23d1ee07c81b2f27490b1b6d3b1121b7c2a2
Peter Tooher <peter.tooher@warwick.ac.uk>
2024-01-03 16:05:07 +0000

    feat(PassportNumberAttribute): add passport number validation in PacificToursApp.Shared namespace
    This commit introduces the PassportNumberAttribute class in the PacificToursApp.Shared namespace. The class extends ValidationAttribute and overrides the IsValid method to validate that a passport number only contains numbers.

---
+++ PacificToursApp/PacificToursApp/Shared/PassportNumberAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PacificToursApp.Shared
+{
+    internal class PassportNumberAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                string passportNumber = value.ToString();
+                Regex regex = new Regex(@"^\d+$");
+                if (!regex.IsMatch(passportNumber))
+                {
+                    return new ValidationResult("Invalid passport number. Only numbers are allowed.");
+                }
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
