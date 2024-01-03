d7b76464447542de6e496a04b88d1e76b5084456
Peter Tooher <peter.tooher@warwick.ac.uk>
2024-01-03 13:30:54 +0000

    feat(UserRegister): add UserRegister class in PacificToursApp.Shared namespace
    This commit introduces the UserRegister class in the PacificToursApp.Shared namespace. The class includes properties for UserName, FirstName, LastName, Email, PassportNumber, ContactNumber, Password, and ConfirmPassword.

---
+++ PacificToursApp/PacificToursApp/Shared/UserRegister.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacificToursApp.Shared
+{
+    public class UserRegister
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public int PassportNumber { get; set; } 
+        public string ContactNumber { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string ConfirmPassword { get; set; } = string.Empty;
+
+    }
+}
