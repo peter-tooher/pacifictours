ede5543eaa1c1efc85c47705201870b9b5d83e15
Peter Tooher <peter.tooher@warwick.ac.uk>
2024-01-03 21:40:52 +0000

    feat(AuthController): implement Register method using AuthService
    This commit implements the Register method in the AuthController class using the AuthService's Register method within the PacificToursApp.Server.Controllers namespace. This implementation allows for user registration functionality.

---
+++ PacificToursApp/PacificToursApp/Server/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PacificToursApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authSevice;
+
+        public AuthController(IAuthService authSevice)
+        {
+            _authSevice = authSevice;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<ServiceResponse<int>>> Register([FromBody]UserRegister request)
+        {
+            var response = await _authSevice.Register(request);
+
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+    }
+}
