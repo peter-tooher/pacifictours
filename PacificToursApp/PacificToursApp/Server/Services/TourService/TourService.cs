4a938b080e6234c449070c6ab9c78f557a0a07e6
Peter Tooher <peter.tooher@warwick.ac.uk>
2024-01-05 16:15:34 +0000

    feat(TourService): Implement GetTourByIdAsync method in TourService class

--- PacificToursApp/PacificToursApp/Server/Services/TourService/TourService.cs
+++ PacificToursApp/PacificToursApp/Server/Services/TourService/TourService.cs
@@ -8,6 +8,23 @@
         {
             _context = context;
         }
+
+        public async Task<ServiceResponse<Tour>> GetTourByIdAsync(int tourId)
+        {
+            var response = new ServiceResponse<Tour>();
+            var product = await _context.Tours.FindAsync(tourId);
+            if (product == null)
+            {
+                response.Success = false;
+                response.Message = "Tour not found.";
+            }
+            else
+            {
+                response.Data = product;
+            }
+            return response;
+        }
+
         public async Task<ServiceResponse<List<Tour>>> GetToursAsync()
         {
             var response = new ServiceResponse<List<Tour>>
